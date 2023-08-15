0ad6a9b15ea9470a544ac7a27b802d6d30bf276b
Abdul Qadir <107254626+abdulqadir3691@users.noreply.github.com>
2023-08-15 11:12:26 +0500

    code pushed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import Header from './component/header/Header';
+import Footer from './component/footer/Footer';
+import Dashboard from './component/dashboard/Dashboard';
+
+function App() {
+  return (
+    <>
+   <Header />
+   <Dashboard />
+   <Footer />
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div>
+            <h1>Welcome to React Class</h1>
+        </div>
+    )
+}
+
+export default Header
